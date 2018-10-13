bf85b96324815dd3372ced8e593752d05f404288
kevin.xu <kevin.xu@icehousecorp.com>
2018-10-13 13:57:30 +0800

    set buildconfig

--- app/src/main/java/com/imaginato/app/kotlinbase/HomeActivity.kt
+++ app/src/main/java/com/imaginato/app/kotlinbase/HomeActivity.kt
@@ -2,11 +2,13 @@ package com.imaginato.app.kotlinbase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        tvContent.setText(BuildConfig.BASE_SERVER)
     }
 }
