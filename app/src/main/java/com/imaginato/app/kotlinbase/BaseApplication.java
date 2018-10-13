a978819297be3b32182c2e47c1e4a2351cccbc40
kevin.xu <kevin.xu@icehousecorp.com>
2018-10-13 14:31:21 +0800

    mutil package name

---
+++ app/src/main/java/com/imaginato/app/kotlinbase/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.imaginato.app.kotlinbase;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by xukai on 2018/10/13.
+ */
+
+public class BaseApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
