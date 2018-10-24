bf3174deecf9cfa73c04e57066989cb4db8452fb
kevin.xu <kevin.xu@imaginato.com>
2018-10-24 23:57:14 +0800

    setVisibleOrGone

--- app/src/main/java/com/imaginato/app/kotlinbase/ui/viewmodel/ImageViewViewModel.kt
+++ app/src/main/java/com/imaginato/app/kotlinbase/ui/viewmodel/ImageViewViewModel.kt
@@ -1,12 +1,20 @@
 package com.imaginato.app.kotlinbase.ui.viewmodel
 
 import android.databinding.BindingAdapter
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.ImageView
 
 /**
  * Created by img on 2018/10/24.
  */
 @BindingAdapter("image")
-fun loadImage(imageView: ImageView, resId:Int){
+fun ImageView.loadImage(imageView: ImageView, resId:Int){
     imageView.setImageResource(resId)
+}
+
+@BindingAdapter("visibleOrGone")
+fun View.setVisibleOrGone(show: Boolean) {
+    visibility = if (show) VISIBLE else GONE
 }
