0e8d71f3276a6f2bba7a086a733d840b04ad1c88
kevin.xu <kevin.xu@imaginato.com>
2018-10-24 12:10:11 +0800

    on click

--- app/src/main/java/com/imaginato/app/kotlinbase/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/imaginato/app/kotlinbase/ui/register/RegisterActivity.kt
@@ -3,9 +3,12 @@ package com.imaginato.app.kotlinbase.ui.register
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.imaginato.app.kotlinbase.R
 import com.imaginato.app.kotlinbase.databinding.ActivityRegisterBinding
 import com.imaginato.app.kotlinbase.model.response.User
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -19,5 +22,15 @@ class RegisterActivity : AppCompatActivity() {
         var plans= arrayListOf<String>()
         plans.add("6Meal")
         binding.plans=plans
+
+        binding.clickListener=object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@RegisterActivity,tv_register.text.toString(),Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    interface PlanClickListener{
+        fun clickListener(planName:String)
     }
 }
